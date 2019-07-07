7f3a9e5f6886ecd55cc11138df691154ae647e50
Jarkko <jarkkohei@luukku.com>
2019-07-07 20:02:47 +0300

    Add a few elements to App.js -file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Styled Components</h1>
+      <form action="">
+        <input type="text"/>
+        <button>Create</button>
+      </form>
     </div>
   );
 }
