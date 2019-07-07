d91b6ea36b77b37743a114cdf6d50a1b2cff8758
Jarkko <jarkkohei@luukku.com>
2019-07-07 20:08:25 +0300

    Import styled-components and create Button-component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import './App.css';
+
+const Button = styled.button`
+  font-family: sans-serif;
+  font-size: 1.3rem;
+  border: none;
+  border-radius: 5px;
+  padding: 7px 10px;
+  background: red;
+  color: #fff;
+`
 
 function App() {
   return (
@@ -7,7 +18,7 @@ function App() {
       <h1>Styled Components</h1>
       <form action="">
         <input type="text"/>
-        <button>Create</button>
+        <Button>Create</Button>
       </form>
     </div>
   );
